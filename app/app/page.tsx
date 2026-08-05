d83b74b99fe789e72c3cb60d0c3039960f2bf2cd
Ishoediah <oeditishaan@gmail.com>
2026-08-04 21:49:25 -0300

    Changed page.tsx. Removed old data and defaulted to show dashboard page on server startup

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,57 +1,6 @@
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
-import { Separator } from "@/components/ui/separator"
-import { SidebarTrigger } from "@/components/ui/sidebar"
+// app/app/page.tsx
+import { redirect } from "next/navigation";
 
-export default function Page() {
-  return (
-    <>
-
-    <h1 className="text-2xl font-bold tracking-tight">beepBoopSR</h1>
-   
-      <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-        <div className="flex items-center gap-2 px-4">
-          <SidebarTrigger className="-ml-1" />
-          <Separator
-            orientation="vertical"
-            className="mr-2 data-vertical:h-4 data-vertical:self-auto"
-          />
-          <Breadcrumb>
-            <BreadcrumbList>
-              <BreadcrumbItem className="hidden md:block">
-                <BreadcrumbLink href="#">
-                  Dashboard test
-                </BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator className="hidden md:block" />
-              <BreadcrumbItem>
-                <BreadcrumbPage>House 1</BreadcrumbPage>
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
-        </div>
-      </header>
-      <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-        
-        <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-
-         
-          <div className="aspect-video rounded-xl bg-muted/50" >
-            <h3 className="font-semibold text-lg flex items-center gap-2">
-              House 1
-            </h3>
-          </div>
-          <div className="aspect-video rounded-xl bg-muted/50" />
-          <div className="aspect-video rounded-xl bg-muted/50" />
-        </div>
-        <div className="min-h-screen flex-1 rounded-xl bg-muted/50 md:min-h-min" />
-      </div>
-    </>
-  )
-  }
+export default function Home() {
+  redirect("/dashboard");
+}
